5f9c7484b33181b66d224bb243a852c4bf0943a3
ShehnazAmeer <48834996+ShehnazAmeer@users.noreply.github.com>
2026-03-19 00:00:12 +0500

    controlled form elements using react-hook-form library

--- src/features/bookings/CreateBooking.jsx
+++ src/features/bookings/CreateBooking.jsx
@@ -4,46 +4,74 @@ import FormLabel from "../../ui/FormLabel";
 import FormRow from "../../ui/FormRow";
 import { useState } from "react";
 import "react-datepicker/dist/react-datepicker.css";
+import useCabin from "../cabins/useCabin";
+import useGuests from "../guests/useGuests";
+import { useForm } from "react-hook-form";
 
 export default function Createbooking() {
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
     const [isBreakfast, setIsBreakfast] = useState(false);
     const [breakfastPaid, setBreakfastPaid] = useState(false);
+    const [cabinId, setCabinId] = useState(null);
+    const[guestId,setGuestId] =useState(null);
+    const { cabins } = useCabin();
+    const { guests } = useGuests();
+    const {register,formState,handleSubmit,reset} =useForm();
+    const cabinIds = cabins?.map(cabin => cabin.id);
+    const guestIds =guests?.map(guest => guest.id); 
 
     return (
         <Form>
             <FormRow style='mb-1'>
-                <FormLabel>Start Date</FormLabel>
+                <FormLabel htmlFor='startDate'>Start Date</FormLabel>
                 <div className="dark:bg-gray-800 dark:text-gray-200 input">
-                    <DatePicker placeholderText="Select Date" className="focus:outline-none focus:ring-2 focus:ring-blue-500" selected={startDate} onChange={date => setStartDate(date)} />
+                    <DatePicker id="startDate" placeholderText="Select Date" className="focus:outline-none focus:ring-2 focus:ring-blue-500" selected={startDate} onChange={date => setStartDate(date)} />
                 </div>
                 
             </FormRow>
             <FormRow style='mb-1'>
-                <FormLabel>End Date</FormLabel>
+                <FormLabel htmlFor='endDate'>End Date</FormLabel>
                 <div className="dark:bg-gray-800 dark:text-gray-200 input">
-                    <DatePicker placeholderText="Select Date" className="focus:outline-none focus:ring-2 focus:ring-blue-500" selected={endDate} onChange={date => setEndDate(date)} />
+                    <DatePicker id='endDate' placeholderText="Select Date" className="focus:outline-none focus:ring-2 focus:ring-blue-500" selected={endDate} onChange={date => setEndDate(date)} />
                 </div>
             </FormRow>
             <FormRow style='mb-1'>
-                <FormLabel>Number of Nights</FormLabel>
+                <FormLabel htmlFor='numNights'>Number of Nights</FormLabel>
                 <input
                     type="number"
+                    id='numNights'
                     className="input"
                     placeholder="Number of Nights"
                 />
             </FormRow>
             <FormRow style='mb-1'>
-                <FormLabel>Number of Guests</FormLabel>
+                <FormLabel htmlFor='numGuests'>Number of Guests</FormLabel>
                 <input
                     className="input"
+                    id='numGuests'
                     type='number'
                     placeholder="Number of Guests"
                 />
             </FormRow>
             <FormRow style='mb-1'>
-                <FormLabel>Cabin Id </FormLabel>
+                <FormLabel htmlFor='cabinId' >Cabin Id </FormLabel>
+                <select
+                    className="input"
+                    id='cabinId'
+                    value={cabinId}
+                    onChange={e=>setCabinId(e.target.value)}
+                >
+                    { cabinIds?.map(id=>(
+                        <option
+                            key={id}
+                            value={id}
+                            className="dark:bg-gray-800 dark:text-gray-200"
+                        >
+                            {id}
+                        </option>) 
+                    )}
+                </select>
                 <input
                     type='text'
                     className="input"
@@ -51,25 +79,58 @@ export default function Createbooking() {
                 />
             </FormRow>
             <FormRow style='mb-1'>
-                <FormLabel>Guest Id</FormLabel>
-                <input
-                    type='text'
-                    className="input"
-                    placeholder="Enter Guest Id"
-                />
-            </FormRow>
-            <FormRow style='mb-1'>
-                <FormLabel>Guest Status</FormLabel>
-                <input
-                    type='text'
-                    className="input"
-                    placeholder="Guest Status"
-                />
-            </FormRow>
-            <FormRow style='mb-1'>
-                <FormLabel>Breakfast Included?</FormLabel>
+                <FormLabel htmlFor='guestId' >Guest Id</FormLabel>
                 <select
                     className="input"
+                    id='guestId'
+                    value={guestId}
+                    onChange={e=>setGuestId(e.target.value)}
+                >
+                    { guestIds?.map(id=>(
+                        <option
+                            key={id}
+                            value={id}
+                            className="dark:bg-gray-800 dark:text-gray-200"
+                        >
+                            {id}
+                        </option>) 
+                    )}
+                </select>
+            </FormRow>
+            <FormRow style='mb-1'>
+                <FormLabel htmlFor='status'>Guest Status</FormLabel>
+                <select
+                    className="input"
+                    id='status'
+                    value={breakfastPaid}
+                    onChange={(e)=>setBreakfastPaid(e.target.value)}
+                >
+                    <option
+                        className="dark:bg-gray-800 dark:text-gray-200"
+                        value='unconfirmed'
+                    >
+                        unconfirmed
+                    </option>
+                    <option
+                        className="dark:bg-gray-800 dark:text-gray-200"
+                        value='checked-in'
+                        
+                    >
+                        checked in
+                    </option>
+                    <option
+                        className="dark:bg-gray-800 dark:text-gray-200"
+                        value='checked-out'
+                    >
+                        checked out
+                    </option>
+                </select>
+            </FormRow>
+            <FormRow style='mb-1'>
+                <FormLabel htmlFor='hasBreakfast' >Breakfast Included?</FormLabel>
+                <select
+                    className="input"
+                    id='hasBreakfast'
                     value={isBreakfast}
                     onChange={(e)=>setIsBreakfast(e.target.value)}
                     
@@ -79,20 +140,22 @@ export default function Createbooking() {
                 </select>
             </FormRow>
             <FormRow style='mb-1'>
-                <FormLabel>Breakfast Paid?</FormLabel>
+                <FormLabel htmlFor='isPaid' >Breakfast Paid?</FormLabel>
                 <select
                     className="input"
+                    id='isPaid'
                     value={breakfastPaid}
                     onChange={(e)=>setBreakfastPaid(e.target.value)}
                 >
-                    <option className="dark:bg-gray-800 dark:text-gray-200" >Yes</option>
-                    <option className="dark:bg-gray-800 dark:text-gray-200">No</option>
+                    <option className="dark:bg-gray-800 dark:text-gray-200" value={true} >Yes</option>
+                    <option className="dark:bg-gray-800 dark:text-gray-200" value={false} >No</option>
                 </select>
             </FormRow>
             <FormRow style='mb-1'>
-                <FormLabel>Observation</FormLabel>
+                <FormLabel htmlFor='observations' >Observation</FormLabel>
                 <textarea
                     className="input"
+                    id='observations'
                     placeholder="Enter Observations"
                 />
             </FormRow>
